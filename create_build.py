979f897b1e6aab445300a5c0b1c4d8d3c1182724
goodguyjay <jair.ramos@live.com>
2024-07-15 19:26:10 -0300

    Edited the Python script to generate the project, added an try catch block to detect any errors and now we're using system_type instead of system_name

--- create_build.py
+++ create_build.py
@@ -1,75 +1,73 @@
 # Dev: Thoni Araujo
+# Edit by: Jair Ramos - 15/07/2024
 
-import platform
+import os
 import pathlib
-
-# returns 'Windows' for windows, or 'Linux'/'Darwin' for unix based systems (Linux, macOS, etc).
-system_name = platform.uname()
-
-# returns a different type of file path depending on the os (Windows or Unix-based)
-file_path = pathlib.Path(__file__)
-
 
 def user_choice() -> str:
     print("1 - Create .sln")
     print("2 - Create make")
     print("3 - Terminate program")
-    return input("Please, select a option (1/2/3): ")
+    return input("Please, select an option (1/2/3): ")
 
-
-def sln_logic():
-    if system_name.system == "Windows":
+def sln_logic(system_type: str):
+    if system_type == "nt":
         content = """
-        call vendor\\bin\\premake\\premake5.exe vs2022\n\nPAUSE
+        call vendor\\bin\\premake\\premake5.exe vs2022
+
+        PAUSE
         """
         file_name = "GenerateProjectWindows.bat"
-        create_file(content.strip(), file_name)
-    elif system_name.system == "Linux" or "Darwin":
+    else:
         content = """
-        ./vendor/bin/premake/premake5 vs2022\n\nread -p "Press [Enter] key to continue..."
+        ./vendor/bin/premake/premake5 vs2022
+
+        read -p "Press [Enter] key to continue..."
         """
         file_name = "GenerateProjectUnix.sh"
-        create_file(content.strip(), file_name)
+    create_file(content.strip(), file_name)
 
-
-def make_logic():
-    if system_name.system == "Windows":
+def make_logic(system_type: str):
+    if system_type == "nt":
         content = """
-        call vendor\\bin\\premake\\premake5.exe gmake2\n\nPAUSE
+        call vendor\\bin\\premake\\premake5.exe gmake2
+
+        PAUSE
         """
         file_name = "GenerateProjectWindowsMake.bat"
-        create_file(content.strip(), file_name)
-    elif system_name.system == "Linux" or "Darwin":
+    else:
         content = """
-        ./vendor/bin/premake/premake5 gmake2\n\nread -p "Press [Enter] key to continue..."
+        ./vendor/bin/premake/premake5 gmake2
+
+        read -p "Press [Enter] key to continue..."
         """
         file_name = "GenerateProjectUnixMake.sh"
-        create_file(content.strip(), file_name)
+    create_file(content.strip(), file_name)
 
-
-def create_file(content, file_name):
+def create_file(content: str, file_name: str):
     try:
-        with open(file_name, "w") as file:
+        file_path = pathlib.Path(file_name)
+        with file_path.open("w") as file:
             file.write(content)
         print(f"{file_name} has been created successfully.")
     except IOError as error:
         print(f"An error occurred while writing to the file: {error}")
 
-
 def main():
+    system_type = os.name
     while True:
         choice = user_choice()
         if choice == '1':
-            sln_logic()
+            sln_logic(system_type)
             break
         elif choice == '2':
-            make_logic()
+            make_logic(system_type)
             break
         elif choice == '3':
             break
         else:
-            print("Invalid option. Please type 1, 2 or 3.")
-
+            print("Invalid option. Please type 1, 2, or 3.")
 
 if __name__ == '__main__':
     main()
+
